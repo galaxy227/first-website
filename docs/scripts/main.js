37fd9b0ecd5bf572c6138f4caeb13eb57956e419
galaxy227 <buck22700@gmail.com>
2025-12-13 03:29:32 -0800

    hamburger button and title refresh

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,5 +1,7 @@
 import * as UTILITY from "./utility.js";
 import * as ARTICLE from "./article.js";
+import * as MENU_DROPDOWN from "./menuDropdown.js";
+import * as TITLE from "./title.js";
 
 /* 
  * ######################
@@ -13,6 +15,12 @@ function init() {
 	for (let i = 0; i < buttonSidebarLeftList.length; i++) {
 		buttonSidebarLeftList[i].addEventListener("click", ARTICLE.handleButtonSidebarLeftClick);
 	}
+	// Menu dropdown
+	const buttonMenuDropdown = document.getElementById("button-menu-dropdown");
+	buttonMenuDropdown.addEventListener("click", MENU_DROPDOWN.handleButtonMenuDropdownClick);
+	// Title
+	const buttonTitle = document.getElementById("button-title");
+	buttonTitle.addEventListener("click", TITLE.handleButtonTitleClick);
 }
 
 /* 
---
+++ docs/scripts/title.js
@@ -0,0 +1,5 @@
+async function handleButtonTitleClick() {
+	window.location.reload();
+}
+
+export { handleButtonTitleClick }
